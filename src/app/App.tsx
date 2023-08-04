9270eaf6a70f9f71612a6f2e56b4da00b4ef1105
SteveNascimento <steve.melo.nascimento@gmail.com>
2023-08-04 13:10:15 -0300

    adding prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,49 @@
+import './app.css';
+import type { MenuProps } from 'antd';
+
+import { Menu, ConfigProvider } from 'antd';
 
 export default function App() {
+  const items: MenuProps['items'] = [
+    {
+      key: 'home',
+      label: 'Home',
+    },
+    {
+      key: 'about',
+      label: 'About',
+    },
+    {
+      key: 'contact',
+      label: 'Contact',
+    },
+  ];
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh'
-      }}
-    >Pastel Mania</div>
-  )
+    <div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#00b96b',
+          },
+        }}
+      >
+        <header
+          style={{
+            backgroundColor: '#FFF9EA',
+            padding: '0 5rem',
+            height: '5rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          {/* Logo */}
+          <img alt='logo' src='https://via.placeholder.com/50' />
+          {/* Menu */}
+          <Menu mode='horizontal' items={items} />
+        </header>
+      </ConfigProvider>
+    </div>
+  );
 }
