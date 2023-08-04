536786f3fe403f426100ade4d3ab52bd136441cc
SteveNascimento <steve.melo.nascimento@gmail.com>
2023-08-04 11:10:11 -0300

    Initial Commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+
+export default function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >Pastel Mania</div>
+  )
+}
